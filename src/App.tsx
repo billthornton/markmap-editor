400c42b65f1835a126a78b97ef377b216ef99547
billthornton <billthornton@users.noreply.github.com>
2021-12-09 23:09:34 +0000

    Basics of an editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,78 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { Transformer } from 'markmap-lib';
+import { Markmap } from 'markmap-view';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const transformer = new Transformer();
+const initValue = `# Title
+
+- one
+- two
+
+`;
+
+
+function Mindmap({ value }: { value: string }) {
+  const svgElementRef = useRef<SVGSVGElement | null>(null);
+  const markMapRef = useRef<Markmap | null>(null);
+
+  useEffect(() => {
+    if (markMapRef.current) return;
+    if (!svgElementRef.current) return;
+
+    markMapRef.current = Markmap.create(svgElementRef.current);
+  }, [svgElementRef.current]);
+
+  useEffect(() => {
+    const markMap = markMapRef.current;
+    const { root } = transformer.transform(value);
+    markMap?.setData(root);
+    markMap?.fit();
+  }, [markMapRef.current, value]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <svg className="mindmap" ref={svgElementRef} />
   );
+}
+
+function Sidebar({ value, onChange }: { value: string, onChange: any }) {
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleChange = (e: any) => {
+    onChange(e.target.value);
+  };
+
+  const handleToggle = (e: any) => {
+    if (e.target.open && textAreaRef.current) {
+      textAreaRef.current.focus();
+    }
+  };
+
+  return (
+    <details className="sidebar" open onToggle={handleToggle}>
+      <textarea
+        ref={textAreaRef}
+        className=""
+        value={value}
+        onChange={handleChange}
+      />
+
+      <summary><b className="summary-text">&#10095;</b></summary>
+    </details>
+  )
+}
+
+
+function App() {
+  const [value, setValue] = useState(initValue);
+
+  return (<>
+    <Sidebar value={value} onChange={setValue} />
+    <Mindmap value={value} />
+  </>)
 }
 
 export default App;
